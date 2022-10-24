9e5997be42dc0b72f66cb0453b91ce5dc2f9aeb0
Try Ajitiono <ballinst@gmail.com>
2022-10-24 07:47:13 +0700

    initial publish

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Test({ importPath }) {
+  const Component = React.lazy(() => {
+    console.info("Attempting to import", `@test-components/${importPath}`);
+    return import(`@test-components/${importPath}`);
+  }).catch(() => {
+    console.error("Component not found");
+    return <div>Component not found</div>;
+  });
+
+  return (
+    <Component numberProp={1} stringProp="helloworld">
+      Test children prop
+    </Component>
+  );
+}
